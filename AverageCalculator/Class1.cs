23b1fc518145528878371dffae81684bf8d8ef57
Student <foobar@sr.htlweiz.at>
2025-12-18 15:01:31 +0100

    Average_Calculator

---
+++ AverageCalculator/Class1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace AverageCalculator;
+    /// <summary>
+    /// Die Klasse AverageCalculator dient zur Speicherung von Zahlenwerte
+    /// und zur Berechnung
+    /// </summary>
+    public class AverageCalculator
+    {
+        /// <summary>
+        /// Enthält alle hinzugefügten Werte
+        /// </summary>
+        private double[] values;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public AverageCalculator()
+        {
+            values = new double[0];
+        }
+
+        public void add(double value)
+        {
+            double[] newArray = new double[values.Length + 1];
+
+            for (int count = 0; count < values.Length; count++)
+            {
+                newArray[count] = values[count];
+            }
+
+            newArray[values.Length] = value;
+            values = newArray;
+        }
+
+        public void add(double[] newValues)
+        {
+            if (newValues == null || newValues.Length == 0)
+            {
+                return;
+            }
+
+            double[] newArray = new double[values.Length + newValues.Length];
+
+            for (int count = 0; count < values.Length; count++)
+            {
+                newArray[count] = values[count];
+            }
+
+            for (int count = 0; count < newValues.Length; count++)
+            {
+                newArray[values.Length + count] = newValues[count];
+            }
+
+            values = newArray;
+        }
+
+        public double getAverage()
+        {
+            if (values.Length == 0)
+            {
+                return 0;
+            }
+
+            double sum = 0;
+
+            for (int count = 0; count < values.Length; count++)
+            {
+                sum += values[count];
+            }
+
+            return sum / values.Length;
+        }
+
+        public double[] getElements()
+        {
+            double[] result = new double[values.Length];
+
+            for (int count = 0; count < values.Length; count++)
+            {
+                result[count] = values[count];
+            }
+
+            return result;
+        }
+
+        public int count()
+        {
+            return values.Length;
+        }
+    }
